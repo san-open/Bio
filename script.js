18664b83add0f1a09d9f93f990b4a6611d07dbdf
Sandeep <87021163+san-open@users.noreply.github.com>
2026-01-24 13:32:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 function toggleMenu() {
   const menu = document.querySelector(".menu-links");
   const icon = document.querySelector(".hamburger-icon");
+
+  if (!menu || !icon) return;
+
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
